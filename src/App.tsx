644759db3f84f7c16ab4ddd901a8324931dd09eb
abheerajput230 <abheerajput230@gmail.com>
2026-02-18 09:06:57 +0530

    asdas

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/context/CartContext";
-import Layout from "@/components/Layout";
-import Products from "./pages/Products";
-import ProductDetail from "./pages/ProductDetail";
-import Checkout from "./pages/Checkout";
-import NotFound from "./pages/NotFound";
+
 import AllPosters from "@/components/AllPosters";
 import CustomFrame from "@/components/CustomFrame";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -20,22 +17,22 @@ const App = () => (
       <CartProvider>
         <Toaster />
         <Sonner />
+
         <BrowserRouter>
           <Routes>
+
+            {/* Home */}
+            <Route path="/" element={<CustomFrame />} />
+
+            {/* All Posters */}
             <Route path="/all-posters" element={<AllPosters />} />
-            <Route path="*" element={
-             
-                <Routes>
-                  <Route path="/" element={<CustomFrame />} />
-                  <Route path="/products" element={<Products />} />
-                  <Route path="/product/:id" element={<ProductDetail />} />
-                  <Route path="/checkout" element={<Checkout />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-            
-            } />
+
+            {/* 404 */}
+            <Route path="*" element={<NotFound />} />
+
           </Routes>
         </BrowserRouter>
+
       </CartProvider>
     </TooltipProvider>
   </QueryClientProvider>
